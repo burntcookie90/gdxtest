a8411f8c0668b96f599037d0e5bc08d9d0599987
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2017-04-03 00:12:04 -0400

    test groups

--- core/src/io/dwak/game/MyGdxGame.kt
+++ core/src/io/dwak/game/MyGdxGame.kt
@@ -6,59 +6,76 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Button
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup
-import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.viewport.FitViewport
 
 class MyGdxGame : ApplicationAdapter() {
   internal lateinit var batch: SpriteBatch
   internal lateinit var img: Texture
-  internal lateinit var stage: Stage
+  internal val stage: Stage by lazy { Stage(FitViewport(1280f, 720f)) }
+  private val skin by lazy { Skin(Gdx.files.internal("default/uiskin.json")) }
 
   override fun create() {
-    stage = Stage(ScreenViewport())
     Gdx.input.inputProcessor = stage
     batch = SpriteBatch()
     img = Texture("badlogic.jpg")
 
-    val skin = Skin(Gdx.files.internal("default/uiskin.json"))
-
-    val rootTable = Table().apply {
-      setFillParent(true)
-      debug = true
-    }.also { stage.addActor(it) }
-
-    val table = Table().apply {
-      debug = true
-      pad(16f, 16f, 16f, 16f)
-    }
     val usernameLabel = Label("Username", skin)
-    val nameText = TextField("", skin)
-    table.add(usernameLabel)
-    table.add(nameText).width(100f)
+    val usernameText = TextField("", skin)
 
     val loginButton = TextButton("Login", skin)
-    table.stack(loginButton).height(150f).width(200f)
-
-    table.row()
+    val exitButton = TextButton("Exit", skin)
 
     val passwordLabel = Label("Password", skin)
     val passwordText = TextField("", skin)
-    table.add(passwordLabel)
-    table.add(passwordText).width(100f)
 
-    val verticalGroup = VerticalGroup()
-    rootTable.add(table.right().top())
+    val usernameGroup = HorizontalGroup().apply {
+      debug = true
+      addActor(usernameLabel)
+      addActor(usernameText)
+      align(Align.center)
+    }
+    val passwordGroup = HorizontalGroup().apply {
+      debug = true
+      addActor(passwordLabel)
+      addActor(passwordText)
+      align(Align.center)
+    }
+
+    val buttonGroup = VerticalGroup().apply {
+      debug = true
+      addActor(loginButton)
+      addActor(exitButton)
+    }
+    val inputGroup = VerticalGroup().apply {
+      debug = true
+      addActor(usernameGroup)
+      addActor(passwordGroup)
+      align(Align.left)
+    }
+
+    val uiGroup = HorizontalGroup().apply {
+      addActor(inputGroup)
+      addActor(buttonGroup)
+      rowAlign(Align.top)
+      setPosition(800f, 600f)
+    }
+    stage.addActor(uiGroup)
+//    stage.addActor(buttonGroup)
+//    rootTable.add(table.right().top())
   }
 
   override fun render() {
-    Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    Gdx.gl.apply {
+      glClearColor(0f, 0f, 0f, 1f)
+      glClear(GL20.GL_COLOR_BUFFER_BIT)
+    }
     batch.begin()
     stage.act(Gdx.graphics.deltaTime)
     stage.draw()
