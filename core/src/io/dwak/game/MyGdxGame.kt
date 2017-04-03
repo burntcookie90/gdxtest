385d431343a635598298b5d93b6039d0968b928a
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2017-04-02 23:27:28 -0400

    test layouting

--- core/src/io/dwak/game/MyGdxGame.kt
+++ core/src/io/dwak/game/MyGdxGame.kt
@@ -6,11 +6,14 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup
 import com.badlogic.gdx.utils.viewport.ScreenViewport
-import io.dwak.game.ext.plusAssign
 
 class MyGdxGame : ApplicationAdapter() {
   internal lateinit var batch: SpriteBatch
@@ -28,18 +31,33 @@ class MyGdxGame : ApplicationAdapter() {
     val rootTable = Table().apply {
       setFillParent(true)
       debug = true
+    }.also { stage.addActor(it) }
+
+    val table = Table().apply {
+      debug = true
+      pad(16f, 16f, 16f, 16f)
     }
+    val usernameLabel = Label("Username", skin)
+    val nameText = TextField("", skin)
+    table.add(usernameLabel)
+    table.add(nameText).width(100f)
 
-    val table = Table().apply { debug = true }
-    val label = Label("Name", skin)
-    table += label
+    val loginButton = TextButton("Login", skin)
+    table.stack(loginButton).height(150f).width(200f)
 
-    rootTable.addActor(table)
-    stage.addActor(rootTable)
+    table.row()
+
+    val passwordLabel = Label("Password", skin)
+    val passwordText = TextField("", skin)
+    table.add(passwordLabel)
+    table.add(passwordText).width(100f)
+
+    val verticalGroup = VerticalGroup()
+    rootTable.add(table.right().top())
   }
 
   override fun render() {
-    Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+    Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
     batch.begin()
     stage.act(Gdx.graphics.deltaTime)
--- core/src/io/dwak/game/ext/TableExt.kt
+++
@@ -1,8 +0,0 @@
-package io.dwak.game.ext
-
-import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-
-operator fun <T: Actor> Table.plusAssign(actor: T) {
-  this.add(actor)
-}
